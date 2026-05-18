38873f38943fca645cb6d673c6accdf01a04f29c
DrBleu <bleuelegantdesigns@gmail.com>
2026-05-17 23:46:45 -0500

    Refactor index.js to remove echo commands

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-echo import React from 'react';
-echo import ReactDOM from 'react-dom/client';
-echo import App from './App';
-echo.
-echo const root = ReactDOM.createRoot(document.getElementById('root'^)^);
-echo root.render(^<App /^>^);
-) > src\index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
