fe32a73dec038c1e3cc14ba6a91824e61fad76ba
DrBleu <bleuelegantdesigns@gmail.com>
2026-05-18 00:47:48 -0500

    Refactor App component and fix import syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-echo import TaxCRM from './components/TaxCRM';
-echo.
-echo export default function App(^) {
-echo   return ^<TaxCRM /^>;
-echo }
-) > src\App.js
+import TaxCRM from './components/TaxCRM';
+
+function App() {
+  return <TaxCRM />;
+}
+
+export default App;
+
