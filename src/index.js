a9d018f20f75f2a42dfe8866571fce9cfe970d39
DrBleu <bleuelegantdesigns@gmail.com>
2026-05-17 23:23:43 -0500

    Initialize index.js with React app entry point

---
+++ src/index.js
@@ -0,0 +1,7 @@
+echo import React from 'react';
+echo import ReactDOM from 'react-dom/client';
+echo import App from './App';
+echo.
+echo const root = ReactDOM.createRoot(document.getElementById('root'^)^);
+echo root.render(^<App /^>^);
+) > src\index.js
